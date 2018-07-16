80ab906036a85425f77543d1a1e2c04f189f61c0
Will Dietz <w@wdtz.org>
2018-07-15 21:51:02 -0500

    llvm2kittel: init

--- default.nix
+++ default.nix
@@ -7,6 +7,10 @@ rec {
   fcd4 = callPackage ./pkgs/fcd/4.nix { };
   fcd4-tests = callPackage ./pkgs/fcd/test.nix { fcd = fcd4; };
 
+  llvm2kittel = callPackage ./pkgs/llvm2kittel {
+    inherit (pkgs.llvmPackages_4) llvm;
+  };
+
   publib = callPackage ./pkgs/publib { };
   slinky = callPackage ./pkgs/slinky { inherit publib; };
   slinky32 = pkgs.pkgsi686Linux.callPackage ./pkgs/slinky { inherit publib; };
