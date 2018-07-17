422e5e03264f33c3ce834e3ead5f18a347fbda32
Will Dietz <w@wdtz.org>
2018-07-17 14:28:46 -0500

    travis: debug

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+set -x
+
 cachix push allvm --watch-store &
 
 nix-build ./.travis/test-release.nix -o result | cachix push allvm
 
+ls -l ./result*
 
 # If triggered by cron, ensure build closure is pushed too
 if [[ $TRAVIS_EVENT_TYPE == "cron" ]]; then
@@ -18,6 +21,7 @@ if [[ $TRAVIS_EVENT_TYPE == "cron" ]]; then
     echo $drvs | xargs nix-store -r 2>&1 | sed -e 's/^/  /'
     echo "computing closure (including outputs....)"
     local closure=$(echo $drvs | xargs nix-store -qR --include-outputs)
+    echo "closure: $closure"
     echo "closure size: $(echo $closure | wc -l)"
     echo "pushing..."
     echo $closure | cachix push allvm 2>&1 | sed -e 's/^/  /'
@@ -25,6 +29,7 @@ if [[ $TRAVIS_EVENT_TYPE == "cron" ]]; then
   }
 
   echo "Pushing build (and runtime) closures..."
+  ls -l ./result*
   push_paths ./result*
 
 else
