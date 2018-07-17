afa1b066bda9ce56bb999ec47593b5aef477a6f2
Will Dietz <w@wdtz.org>
2018-07-17 11:28:52 -0500

    travis: enable cachix pushing, push built outputs

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#cachix push allvm --watch-store &
+cachix push allvm --watch-store &
 
-nix-build ./.travis/test-release.nix -o result #| cachix push allvm
+nix-build ./.travis/test-release.nix -o result | cachix push allvm
 
-#cachix push allvm ./result
+cachix push allvm ./result*
