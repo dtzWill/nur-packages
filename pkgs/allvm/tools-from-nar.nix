3359e2fe4442d1803a233f45c73f42cff523deb5
Will Dietz <w@wdtz.org>
2018-07-20 16:06:52 -0500

    allvm-tools: add version

--- pkgs/allvm/tools-from-nar.nix
+++ pkgs/allvm/tools-from-nar.nix
@@ -1,13 +1,17 @@
 { lib }:
-(lib.from-nar {
-  name = "allvm-tools-bin";
-  narurl = "nar/0f77fc768873b0a53b40ca034404a814085650ab74fd687067effda9198fdd58.nar.xz";
-  narHash = "sha256:0hnr7awn64a9kjninnbkcav9cl3k2bwkxk25qk0z9dk1f69qf36r";
-}) // {
+let 
+  name = "allvm-tools-${version}";
+  version = "2018-05-22-bf6536b";
+  unpacked = lib.from-nar {
+    inherit name;
+    narurl = "nar/0f77fc768873b0a53b40ca034404a814085650ab74fd687067effda9198fdd58.nar.xz";
+    narHash = "sha256:0hnr7awn64a9kjninnbkcav9cl3k2bwkxk25qk0z9dk1f69qf36r";
+  };
   meta = with lib; {
     description = "ALLVM Tools (multiplexed)";
     maintainers = with maiintainers; [ dtzWill ];
     license = licenses.ncsa;
     homepage = https://github.com/allvm/allvm-tools;
   };
-}
+in
+  unpacked // { inherit name meta; }
