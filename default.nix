2723baa9da3fa5502a7141b94393ac3eac3a2599
Will Dietz <w@wdtz.org>
2018-07-20 12:23:52 -0500

    svf: add tests, ptaben expression

--- default.nix
+++ default.nix
@@ -72,4 +72,14 @@ rec {
   svf_4 = callPackage ./pkgs/svf/4.nix { llvm = pkgs.llvm_4; };
   svf_6 = callPackage ./pkgs/svf { llvm = pkgs.llvm_6; };
   svf = svf_4;
+  svf4-tests = import ./pkgs/svf/test.nix {
+    inherit (pkgs.llvmPackages_4) llvm clang;
+    svf = svf_4;
+    inherit (pkgs) runCommand graphviz makeFontsConf freefont_ttf;
+  };
+  ptaben-fi_4 = callPackage ./pkgs/svf/ptaben.nix {
+    inherit (pkgs.llvmPackages_4) llvm clang;
+    svf = svf_4;
+  };
+  ptaben-fs_4 = ptaben-fi_4.override { testFSPTA = true; };
 }
---
+++ pkgs/svf/test.nix
@@ -0,0 +1,22 @@
+{ runCommand, svf, llvm, clang, graphviz, makeFontsConf, freefont_ttf }:
+
+
+let
+  swapbc = runCommand "swapbc" {
+    buildInputs = [ llvm clang ];
+    hardeningDisable = [ "all" ];
+  } ''
+    clang -c -emit-llvm ${./test.c} -o swap.bc
+    opt -mem2reg swap.bc -o swap.opt
+    mkdir -p $out
+    mv swap.opt $out/
+  '';
+  utils = import ./utils.nix {
+    inherit runCommand svf graphviz makeFontsConf freefont_ttf;
+  };
+
+in with utils; {
+  # inherit analyze_fn;
+
+  swap = analyze_fn "${swapbc}/swap.opt";
+}
