d57455d3194a996a74fa0b7dbd425fd0a5831899
Will Dietz <w@wdtz.org>
2018-07-30 14:26:45 -0500

    svf: 2018-07-03 -> 2018-07-22

--- pkgs/svf/default.nix
+++ pkgs/svf/default.nix
@@ -2,12 +2,12 @@
 
 let
   srcinfo = {
-    version = "2018-07-03";
+    version = "2018-07-22";
     src = fetchFromGitHub {
       owner = "SVF-tools";
       repo = "SVF";
-      rev = "b7ddf981ae1e3be79eab691f6cf844313ee66b77";
-      sha256 = "0f47579hlbdb1mivxc2l91xvf0577v8n4x4vmgr76nn2nqq7s75q";
+      rev = "31c86b143fc6796bb7f9c51645217f1c09d7e8a5";
+      sha256 = "1lcb42szp28bp08v016k38gl64l31vqahcdy664sb980vbg8j8b5";
     };
   };
 in import ./generic.nix { inherit stdenv llvm cmake srcinfo; } {
