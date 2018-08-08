518826ed43abdd4b382917447a3e8edc0e42f9fd
Will Dietz <w@wdtz.org>
2018-08-08 10:12:45 -0500

    llstrata: use github sources instead, wrangle deps, fix!

--- pkgs/llstrata/default.nix
+++ pkgs/llstrata/default.nix
@@ -1,38 +1,40 @@
-{ stdenv, rustPlatform, llvm, clang, gcc, fetchgit, libffi }:
+{ stdenv, fetchFromGitHub, rustPlatform, llvm, clang, gcc, libffi }:
 
-with import ./src.nix;
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   name = "llstrata-${version}";
-  inherit version;
-  src = fetchgit srcinfo;
+  version = "2017-08-10";
+  src = fetchFromGitHub {
+    owner = "jcranmer";
+    repo = "llstrata";
+    rev = "869c980a32b4e4e7b05204a05de5a23aa3e897f4";
+    sha256 = "1hk4b3fdjpnq9hvvwwnirq3m0xfcpycxgpz6f5cqiq54r2zya39j";
+  };
 
-  buildInputs = [ llvm clang libffi ];
+  buildInputs = [ llvm clang.cc.lib clang libffi ];
 
   cargoSha256 = "04xad7sa2bkhl1zbfa3i8wfhlpii0kk9zphmgjix0achc77jbha0";
 
+  postUnpack = ''
+    cat >> .cargo/config <<EOF
+    [source."https://github.com/jcranmer/llvm-rs"]
+    git = "https://github.com/jcranmer/llvm-rs"
+    branch = "master"
+    replace-with = "vendored-sources"
+    EOF
+  '';
+  
+  prePatch = ''
+    for x in build.rs llvm-mc/build.rs; do
+      substituteInPlace $x \
+        --replace 'version != "5.0.0svn"' '!version.starts_with("5.0.")'
+    done
+  '';
+
   configurePhase = ''
-    export LIBCLANG_PATH="${clang.cc}/lib"
+    export LIBCLANG_PATH="${clang.cc.lib}/lib"
     export CC=${gcc}/bin/cc
     export CXX=${gcc}/bin/c++
   '';
 
-  preBuild = ''
-    for c in $(find . -name Cargo.toml); do
-      # Extract from 'name = "$NAME"'
-      proj=$(sed -n 's/name = "\(.*\)"/\1/p' $c)
-      echo "Running cargo build --release -p $proj"
-      cargo build --release -p $proj
-    done
-  '';
-
   doCheck = true;
-
-  checkPhase = ''
-    for c in $(find . -name Cargo.toml); do
-      # Extract from 'name = "$NAME"'
-      proj=$(sed -n 's/name = "\(.*\)"/\1/p' $c)
-      echo "Running cargo test -p $proj"
-      cargo test -p $proj
-    done
-  '';
 }
