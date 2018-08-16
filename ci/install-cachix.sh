c482eb30cd15c0c23c280a7de12f387623afc76b
Will Dietz <w@wdtz.org>
2018-08-16 01:36:48 -0500

    ci: install cachix using pinned nixpkgs mention in upstream's README

--- ci/install-cachix.sh
+++ ci/install-cachix.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-nix-env -if https://github.com/NixOS/nixpkgs/tarball/master -A cachix
+nix-env -iA cachix -f https://github.com/NixOS/nixpkgs/tarball/1d4de0d552ae9aa66a5b8dee5fb0650a4372d148
