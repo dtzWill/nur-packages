c896ae44bce1955c90f3d2694a2ee76a1dd289e2
Will Dietz <w@wdtz.org>
2018-08-20 20:26:29 -0500

    iosevka: pname in dir

--- pkgs/iosevka-term/default.nix
+++ pkgs/iosevka-term/default.nix
@@ -10,7 +10,7 @@ let
 
     postFetch = ''
       mkdir -p $out/share/fonts/
-      unzip -j $downloadedFile ttf/\*.ttf -d $out/share/fonts/iosevka
+      unzip -j $downloadedFile ttf/\*.ttf -d $out/share/fonts/${pname}
     '';
 
     inherit sha256;
@@ -30,15 +30,15 @@ let
   };
 in {
   # See https://raw.githubusercontent.com/be5invis/Iosevka/master/images/stylesets.png
-  ss01 = mkSS "01" "00qg16m3h97lvdy9gay04pfkyn8vdgg0sh681v20sd6jd045nc4y";
-  ss02 = mkSS "02" "0rcfsbcvmb4wi90b4w74w534cchlsbrxvwbsrxhzz5l0bbcghbsg";
-  ss03 = mkSS "03" "0m3xr7h877nh1n76rmngavdd5vars2qxnax6w88pl0gqjxzqcvn7";
-  ss04 = mkSS "04" "1nm2x53njp1fsih2fapqb4h4aaw5g22y3g928ijrjhzgf1apmwdx";
-  ss05 = mkSS "05" "0inj5qzr9q7mdvfwk2azy07vwazlwgnmyzggf347ap3z9fp7wng9";
-  ss06 = mkSS "06" "0lva1m3adrfniyj4xxkiqxv18zny9447b8k1smf1cc1nddbiais4";
-  ss07 = mkSS "07" "088ya124is2458pr3icyjf76cai8v9ixyq9i7kj2fmmdwmy75dj6";
-  ss08 = mkSS "08" "192rdhl1a56h66xnv8k4gqd1v7sybl93iphgd0fvgfw4lyzcl4pj";
-  ss09 = mkSS "09" "1ziwgjd6wxq4d7l6b3i8k43wbcxbfals29xhbizfj2lh0bjqfr8m";
-  ss10 = mkSS "10" "1h4wqnbpxbihzddwyv046mrmiw2d1a54c6pxdvncp0iv4r4iq9g9";
-  ss11 = mkSS "11" "08ccpqkx5vm2c9iv8fc7l2h0xiqap05jqc6wwpkg8klpxscsrr95";
+  ss01 = mkSS "01" "0banyj4lpr0nnnarm42vkckasgagqd41q8r4ksy0y16ggidb61v6";
+  ss02 = mkSS "02" "0ivpa69yzp0b6gg8hyi632v51jznjr789i76sanwggz0h6hjbyxj";
+  ss03 = mkSS "03" "0yfbrvfpxanl4d5ys9kg2xybdbncaxcd44sf57z6akawk8d7bpm7";
+  ss04 = mkSS "04" "1vqdp9nn2x1r95xs4nhnp7fxnhsfl1hddlcs1djmqg0pxp4p1v52";
+  ss05 = mkSS "05" "1ccmn4a9drzp2w6cyj2dgl6v8857ms8bml09f5qpp126f58f7dmw";
+  ss06 = mkSS "06" "128p9r9m289svdqknqdw8p4408lyp656j03wdrazmxbigh6hd8q3";
+  ss07 = mkSS "07" "0cvgp1y32x9g6rrrgj2ag9sidqpfhjyi4fa9rn38vplzl04v9k67";
+  ss08 = mkSS "08" "1498747cy2vvpmlzigsbpz3167v8qhjb4hjb2yrqbakbx5wssh25";
+  ss09 = mkSS "09" "1qa4x4gsqd99blgfyifxwxayn5vq4ifnf5vi5856jzfp0qqas9rv";
+  ss10 = mkSS "10" "1vmx9933x90w51r43bjn37dvhbpr9y0invvbf2616kir9wl4nk72";
+  ss11 = mkSS "11" "0az1fsd63p5kz5j956ywnxnw6jmm60b1q3f9bn9q6kblcrqsjgwc";
 }
