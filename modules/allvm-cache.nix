05cb917ca7efce6076a5c12ecaa28898caddd19d
Will Dietz <w@wdtz.org>
2018-09-19 18:14:55 -0500

    allvm-{cache,cachix}: init

---
+++ modules/allvm-cache.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.allvm;
+in {
+  options.allvm = {
+    useDirectS3URL = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Use Direct S3 for ALLVM cache. Hydra builders should use this to avoid CloudFront's negative cache.";
+    };
+  };
+
+  config = {
+    # For the channel, not the cache
+    nix.extraOptions = ''
+      netrc-file = ${./allvm-netrc}
+    '';
+
+    nix.binaryCaches = [
+      "https://cache.nixos.org/"
+      (if cfg.useDirectS3URL then
+        "https://s3.amazonaws.com/cache.allvm.org/"
+      else
+        "https://cache.allvm.org/"
+      )
+    ];
+
+    nix.binaryCachePublicKeys = [
+      "gravity.cs.illinois.edu-1:yymmNS/WMf0iTj2NnD0nrVV8cBOXM9ivAkEdO1Lro3U="
+    ];
+  };
+}
--- modules/default.nix
+++ modules/default.nix
@@ -1,3 +1,5 @@
 {
+  allvm-cache = ./allvm-cache.nix; # largely deprecated in favor of cachix
+  allvm-cachix = ./allvm-cachix.nix;
   vim-manpager = ./vim-manpager.nix;
 }
