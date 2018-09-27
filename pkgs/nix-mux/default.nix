fa9cd04d88ed3090f6d8cbeb9773800bf1811451
Will Dietz <w@wdtz.org>
2018-09-27 18:48:04 -0500

    nix-mux: bump

--- pkgs/nix-mux/default.nix
+++ pkgs/nix-mux/default.nix
@@ -1,11 +1,11 @@
 { stdenv, runCommandNoCC, lib }:
 
 let
-  version = "2.2dtz-mux-28cb9c6";
+  version = "2.2dtz-mux-4446425";
   nix-mux-tarball = lib.from-nar {
     name = "nix-${version}";
-    narurl = "nar/72694cb5b64d4f2ff8fbc9d5aa40b5b819b702743dd7536ed7dbc30688b02661.nar.xz";
-    narHash = "sha256:0lfp7hzq43c3gvvq6qb21q9mqjfdmwpgcmysnsad51cnvnac5wgs";
+    narurl = "nar/a43c1ca47cc343f42ea02d8125581f8be300108ce7fddd633bb7ae640a09ebc9.nar.xz";
+    narHash = "sha256:005nzr7pw3ds1mb0ni6pyi9bljalvljazf0ykmckiiriwih4s9w9";
   };
 
   bootstrapFiles = {
