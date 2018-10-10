f7afd93d3563c795c1557d92805ccf01a0b5effc
Will Dietz <w@wdtz.org>
2018-10-10 08:46:47 -0500

    xapian: fixy

--- overlays/xapian.nix
+++ overlays/xapian.nix
@@ -2,12 +2,19 @@ self: super: {
   xapian = super.xapian.overrideAttrs(o: rec {
     name = "xapian-${version}";
     version = "2018-10-09";
-    src = super.fetchFromGitHub {
-      owner = "xapian";
-      repo = "xapian";
+    src = super.fetchgit {
+      url = "https://github.com/xapian/xapian";
       rev = "4ca08213fcf5eca4d844cb522cad18235c81875c";
-      sha256 = "0g40bdci54049myjvdb7dr95hbj8rk4hjg406lsibydi067w19gz";
+      sha256 = "168whd1lf4jm5w601ak805g3jzj54n44sh1cyfs1lba8vm9wfpbx";
+      leaveDotGit = true;
     };
     patches = null; # Drop patch from upstream included by using latest
+
+    nativeBuildInputs = (o.nativeBuildInputs or []) ++ [ self.perl ];
+
+    preAutoreconf = ''
+      patchShebangs bootstrap */preautoreconf
+      ./bootstrap
+    '';
   });
 }
