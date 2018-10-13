0482db1e2c9accfec08fc6cfcf03862e16c69bfe
Will Dietz <w@wdtz.org>
2018-10-12 23:29:43 -0500

    fwupd: nvm still needs appstream-glib, I misread commits

--- overlays/fwupd.nix
+++ overlays/fwupd.nix
@@ -10,20 +10,5 @@ self: super: {
       sha256 = "0b7l1v1sirxz04pf8g8mid6kpyyzxbazjj15crmb3brzv8jpz5fa";
     };
 
-    # Original:
-    #buildInputs = [
-    #  polkit appstream-glib gusb sqlite libarchive libsoup elfutils libsmbios gnu-efi libyaml
-    #  libgudev colord gpgme libuuid gnutls glib-networking efivar json-glib umockdev
-    #  bash-completion
-    #];
-    buildInputs = with self; [
-      # Removed:
-      # appstream-glib
-      # Remaining:
-      polkit gusb sqlite libarchive libsoup elfutils libsmbios gnu-efi libyaml
-      libgudev colord gpgme libuuid gnutls glib-networking efivar json-glib umockdev
-      bash-completion
-    ];
-
   });
 }
