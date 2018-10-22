2801ded9ad5933e6baad70e2efa383255c647eef
Will Dietz <w@wdtz.org>
2018-10-21 19:24:15 -0500

    svfPkgs_{6,master}: revert source as it the commit no longer exists
    Effectively reverts 50874cf6e97cdacd11eb4e8b3594a333061529bf,
    but accommodating refactoring since.
    
    Looks like upstream deleted a tag as well,
    so they likely force-pushed to revert the commit
    we were using as well.
    
    Let's take the hint -- and fix our builds :grin: --
    and use the "old" (yet currently latest) version instead.

--- pkgs/svf/master.nix
+++ pkgs/svf/master.nix
@@ -2,12 +2,12 @@
 
 let
   srcinfo = {
-    version = "2018-10-21";
+    version = "2018-10-19";
     src = fetchFromGitHub {
       owner = "SVF-tools";
       repo = "SVF";
-      rev = "21c4b6a1d82fbad32b6b3d2701365774c70cb4d1";
-      sha256 = "1kyn3sxnx5sd0bx71gzrccrvxscw5a25mnvh5kfdpyj7yy3jzndw";
+      rev = "8607025bb1ec0f69f6df8f6d163f43bc9cf502ac";
+      sha256 = "0s0cp5ykkj6p79xz6xxbkj5sibasjih1kh3xfs671asj18g0rj3k";
     };
   };
 in import ./generic.nix { inherit stdenv llvm cmake srcinfo; } {
