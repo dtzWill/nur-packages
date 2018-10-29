539f9b53169ba3681667c0db9fa6cb8780e18bf7
Will Dietz <w@wdtz.org>
2018-10-29 07:45:36 -0500

    chelf: fix a few missed chstk -> chelf renames

--- pkgs/chelf/default.nix
+++ pkgs/chelf/default.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation rec {
 
   src = fetchFromGitHub {
     owner = "Gottox";
-    repo = "chstk";
+    repo = "chelf";
     rev = "c3204f80e9db350f2ba6498b684356f7ea505fa5";
     sha256 = "1nfryd8r38amnak2fqj6g0kh79l4yzg1zfg6c2ggn0xyx1n4kks8";
   };
@@ -22,7 +22,7 @@ stdenv.mkDerivation rec {
 
   meta = with stdenv.lib; {
     description = "change or display the stack size of an ELF binary";
-    homepage = https://github.com/Gottox/chstk;
+    homepage = https://github.com/Gottox/chelf;
     license = licenses.bsd2;
     maintainers = with maintainers; [ dtzWill ];
   };
