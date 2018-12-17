a0a83ae036ae7b28aefae6c4714a8d7b8e4ef47f
Will Dietz <w@wdtz.org>
2018-12-17 14:15:40 -0600

    tlp: specify func.d location, needed now

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -8,5 +8,9 @@ self: super: {
       rev = "570cd7a9100585621cece30d03149617997c4c7e";
       sha256 = "09nsfmayxng0mc9jg1nf8k05aq3v93xwch4kjf60qrpxm4pdg6gs";
     };
+
+    makeFlags = (o.makeFlags or []) ++ [
+      "TLP_FLIB=${placeholder "out"}/share/tlp/func.d"
+    ];
   });
 }
