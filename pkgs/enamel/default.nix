84e296468ad84ad1798015437dbdfffe33fa712b
Will Dietz <w@wdtz.org>
2018-12-22 20:44:57 -0600

    enamel: add deps

--- pkgs/enamel/default.nix
+++ pkgs/enamel/default.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, rustPlatform }:
+{ lib, fetchFromGitHub, rustPlatform, sassc, glib, gtk3, notmuch, libsoup, gmime3 }:
 
 rustPlatform.buildRustPackage rec {
   name = "enamel-${version}";
@@ -10,6 +10,8 @@ rustPlatform.buildRustPackage rec {
     rev = "1bff1f498eb64ba173851cf75172c54be037939a";
     sha256 = "1qw1qymrnx1ac0qk95q84jq25h41a64whmfmxdv9yrxwfhs3fqnb";
   };
+
+  buildInputs = [ sassc glib gtk3 notmuch libsoup gmime3 ];
 
   cargoPatches = [
     ./0001-Don-t-look-for-deps-locally-that-don-t-generally-exi.patch
