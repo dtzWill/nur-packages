b4207d6e025045c8546cd5f22bd15cf4f1e2a660
Will Dietz <w@wdtz.org>
2018-12-22 21:44:02 -0600

    enamel: patch out custom_derive

--- pkgs/enamel/default.nix
+++ pkgs/enamel/default.nix
@@ -33,6 +33,11 @@ in rustPlatform.buildRustPackage rec {
     ./0002-Cargo.lock-init.patch
   ];
 
+  postPatch = ''
+    substituteInPlace enamel-core/src/lib.rs \
+     --replace '#![feature(custom_derive)]' ""
+  '';
+
   cargoSha256 = "1hlm1pp9917ri9xr72qg43yx7cf2g4hx3gx060cr102czj06c4i1";
 }
 
