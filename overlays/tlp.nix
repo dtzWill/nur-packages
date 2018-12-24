e42a843f3edc3bd5be8b8d3fa3a0bad8dd855ce0
Will Dietz <w@wdtz.org>
2018-12-24 02:05:19 -0600

    Revert "tlp: 2018-12-17 -> 2018-12-21"
    breaks udev rules, revisit
    
    This reverts commit 6823263a1d7e8b415eadf6b0bac9b686b2b34b7e.

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -1,12 +1,12 @@
 self: super: {
   tlp = super.tlp.overrideAttrs (o: rec {
     name = "tlp-${version}";
-    version = "2018-12-21";
+    version = "2018-12-17";
     src = super.fetchFromGitHub {
       owner = "linrunner";
       repo = "tlp";
-      rev = "264a98b29edb2da10bcb622d7ecbeff65e21bdc2";
-      sha256 = "0nj2xnd7c4fqpibqv9fpwxkrlv5rk3aw9a2rhsvj80vpkc89qz49";
+      rev = "570cd7a9100585621cece30d03149617997c4c7e";
+      sha256 = "09nsfmayxng0mc9jg1nf8k05aq3v93xwch4kjf60qrpxm4pdg6gs";
     };
 
     makeFlags = (o.makeFlags or []) ++ [
