7075efe176fba88f778726650c125d0cd31551f0
Will Dietz <w@wdtz.org>
2019-01-27 22:14:29 -0600

    awesome-git: 2019-01-24 -> 2019-01-27

--- overlays/awesome-git.nix
+++ overlays/awesome-git.nix
@@ -1,13 +1,13 @@
 self: super: {
   awesome = super.awesome.overrideAttrs (o: rec {
     name = "awesome-4.2-git-${version}";
-    version = "2019-01-24";
+    version = "2019-01-27";
     nativeBuildInputs = o.nativeBuildInputs or [] ++ [ self.asciidoctor ];
     src = super.fetchFromGitHub {
       owner = "AwesomeWM";
       repo = "awesome";
-      rev = "7f0e2e6bbf30bd6c5aba61bc4fbb06286c9f9d82";
-      sha256 = "06g962dd4495q1nsd64rfgnp2rp7x2453x93b8dzdfgjgxqgpcva";
+      rev = "febbbb69a505bf20e26563467f04f5c75176c1b9";
+      sha256 = "0ixq14lpwrg6djskk7kjqlawg1bvqq68b8vdqlv5khzvjdl4wdc3";
     };
   });
 }
