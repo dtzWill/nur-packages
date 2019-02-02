5f00f0e5795e4a57b7b37453bcf3be0d22fd6655
Will Dietz <w@wdtz.org>
2019-02-01 23:51:33 -0600

    gxi: look for plugins in slightly better place, add syntect
    gxi bundles syntect, but not sure how to get it installed
    and since already have syntect from xi-core let's try that.
    
    GXI_PLUGIN_DIR gets baked into the program,
    but better a path from $out than /usr/whatever

--- pkgs/xi/gxi/default.nix
+++ pkgs/xi/gxi/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub, rustPlatform, cmake, pkgconfig, freetype, gtk3, wrapGAppsHook }:
+{ stdenv, fetchFromGitHub, rustPlatform, cmake, pkgconfig, freetype, gtk3, wrapGAppsHook, xi-core }:
 
 rustPlatform.buildRustPackage rec {
   name = "gxi-unstable-${version}";
@@ -19,9 +19,16 @@ rustPlatform.buildRustPackage rec {
     wrapGAppsHook
   ];
 
+  GXI_PLUGIN_DIR = "${placeholder "out"}/share/xi/plugins";
+
   hardeningDisable = [ "format" ]; # build error in gettext/gnulib??
 
   cargoSha256 = "1ani8xm55mgrrf5m2va26m5c75qrql9k4l5i05rhrbvn9y37c881";
+
+  postInstall = ''
+    mkdir -p ${GXI_PLUGIN_DIR}
+    ln -s ${xi-core.syntect} ${GXI_PLUGIN_DIR}/synctect
+  '';
 
   meta = with stdenv.lib; {
     description = "GTK frontend for the xi text editor, written in rust";
