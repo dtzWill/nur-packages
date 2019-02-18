c42cb5298a489fb42cbd2426acf65c7281c2ee11
Will Dietz <w@wdtz.org>
2019-02-18 13:14:58 -0600

    gxi: 2019-02-14 -> 2019-02-18

--- pkgs/xi/gxi/default.nix
+++ pkgs/xi/gxi/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "gxi-unstable-${version}";
-  version = "2019-02-14";
+  version = "2019-02-18";
   
   src = fetchFromGitHub {
     owner = "Cogitri";
     repo = "gxi";
-    rev = "4d9aff7ea072498c27dab6dc24a92b6dee0ed5f8";
-    sha256 = "10dbkcnbil2rl58aj6z7xn8i4sf55kpw98d7xmb5aq0j2zr03bg1";
+    rev = "edba6fd994dbc66649d1c24f8db8925ca025eb1f";
+    sha256 = "";
     fetchSubmodules = true;
   };
 
