00226aefe47c8ce72490648fbcb21db395830376
Will Dietz <w@wdtz.org>
2019-03-07 12:38:20 -0600

    awesome: provide xcb-errors, new optional dep

--- overlays/awesome-git.nix
+++ overlays/awesome-git.nix
@@ -10,5 +10,6 @@ self: super: {
       rev = "795c792d118e29621c59d0664318f199c80caf93";
       sha256 = "1p4p4v7f9rd6hqmzvq8ba6xz45i5a5z66ghmmx28j1jxhmq56bg9";
     };
+    buildInputs = (o.buildInputs or []) ++ [ self.xorg.xcbutilerrors ];
   });
 }
