b14eb4604c470e4bc114bab96ef7192b8359428c
Will Dietz <w@wdtz.org>
2019-03-08 14:09:37 -0600

    samurai: init at 0.6

---
+++ pkgs/samurai/default.nix
@@ -0,0 +1,19 @@
+{ stdenv, fetchFromGitHub }:
+
+stdenv.mkDerivation rec {
+  pname = "samurai";
+  version = "0.6";
+
+  src = fetchFromGitHub {
+    owner = "michaelforney";
+    repo = pname;
+    rev = version;
+    sha256 = "1m4yjkyjm278399hj02vs6nzpll1zxr72c62rhx08gsq755q3ncs";
+  };
+
+  meta = with stdenv.lib; {
+    description = "ninja-compatible build tool written in C";
+    license = with licenses; [ mit asl20 ]; # see LICENSE
+    maintainers = with maintainers; [ dtzWill ];
+  };
+}
