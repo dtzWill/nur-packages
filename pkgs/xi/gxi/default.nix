197d1182ff854491bfde966d10b9e92ebbf328f8
Will Dietz <w@wdtz.org>
2019-03-13 10:34:11 -0500

    gxi: hashfixes

--- pkgs/xi/gxi/default.nix
+++ pkgs/xi/gxi/default.nix
@@ -8,7 +8,7 @@ rustPlatform.buildRustPackage rec {
     owner = "Cogitri";
     repo = "gxi";
     rev = "44c0065d748a4325a911b7b71b1ca9a310f1fc70";
-    sha256 = "";
+    sha256 = "1nx3bg6sr0x3ndgqfiz4iiki96y1inwyb2fyw6ai3lkvqzp2p4qd";
     fetchSubmodules = true;
   };
 
@@ -23,7 +23,7 @@ rustPlatform.buildRustPackage rec {
 
   hardeningDisable = [ "format" ]; # build error in gettext/gnulib??
 
-  cargoSha256 = "0cgwliix76azs2qwqj43z60jhm78l9xy4bk9a945yrnpy8f4wjmx";
+  cargoSha256 = "0q83k1m0d0hq307psi9l74ll4ksd1myxgnl09g0yarr0948fjgwg";
 
   postInstall = ''
     mkdir -p ${GXI_PLUGIN_DIR}
