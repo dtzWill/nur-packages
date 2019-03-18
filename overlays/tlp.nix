3d591cce37efb2e8e6f3311d096fa9eb39ec459e
Will Dietz <w@wdtz.org>
2019-03-18 12:30:56 -0500

    tlp: 1.2 -> 1.2.1

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -2,13 +2,13 @@ self: super: {
   tlp = super.tlp.overrideAttrs (o: rec {
     name = "tlp-${version}";
     #version = "2019-03-09";
-    version = "1.2";
+    version = "1.2.1";
     src = super.fetchFromGitHub {
       owner = "linrunner";
       repo = "tlp";
       #rev = "527b0e1acbf77dad13e973bd2a98412acadd5e7f";
       rev = version;
-      sha256 = "0vabhz3iil0danil0s88c6z97cycl03s2y948h8s9fmcgkh34gwd";
+      sha256 = "1msldl6y8fpvxa9p87lv3hvgxwk2vpiahqmapq485ihdjkshc558";
     };
 
     makeFlags = (o.makeFlags or []) ++ [
