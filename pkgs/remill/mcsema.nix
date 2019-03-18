78c4d1be6e54e3f7a382571c057148f5050f8704
Will Dietz <w@wdtz.org>
2019-03-18 18:03:19 -0500

    mcsema: 2019-03-13 -> 2019-03-18

--- pkgs/remill/mcsema.nix
+++ pkgs/remill/mcsema.nix
@@ -1,12 +1,12 @@
 { fetchFromGitHub }:
 
 {
-  version = "2019-03-13";
+  version = "2019-03-18";
   src = fetchFromGitHub {
     owner = "trailofbits";
     repo = "mcsema";
-    rev = "a8176f141f570e4e423655a8416fe61d01137377";
-    sha256 = "1lnali60ifddhwyjgdsxs4x61whscqgjqrrpicwlbf8vj10ynfkz";
+    rev = "b8146a30e4c04fbc4771d6abae0a5aacbfb8df06";
+    sha256 = "";
     name = "mcsema-source";
   };
 }
