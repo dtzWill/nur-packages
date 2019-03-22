713e27d1852d4664e8705e57241efb7d2b2c7cec
Will Dietz <w@wdtz.org>
2019-03-22 00:02:35 -0500

    mcsema: restore hash, whoops
    update script gone wrong, probably?

--- pkgs/remill/mcsema.nix
+++ pkgs/remill/mcsema.nix
@@ -6,7 +6,7 @@
     owner = "trailofbits";
     repo = "mcsema";
     rev = "b8146a30e4c04fbc4771d6abae0a5aacbfb8df06";
-    sha256 = "";
+    sha256 = "057p7fcyl728hjviw6qy13psmbg0hgll6myq2jks74r2c94mcr1h";
     name = "mcsema-source";
   };
 }
