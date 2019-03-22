1e587242154294189c5f77ee6b62485f94b0759b
Will Dietz <w@wdtz.org>
2019-03-22 04:33:02 -0500

    mcsema: change name slightly to force new store path
    Seems there's a corrupt path in a cache?

--- pkgs/remill/mcsema.nix
+++ pkgs/remill/mcsema.nix
@@ -2,11 +2,11 @@
 
 {
   version = "2019-03-18";
-  src = fetchFromGitHub {
+  src = fetchFromGitHub rec {
     owner = "trailofbits";
     repo = "mcsema";
     rev = "b8146a30e4c04fbc4771d6abae0a5aacbfb8df06";
     sha256 = "057p7fcyl728hjviw6qy13psmbg0hgll6myq2jks74r2c94mcr1h";
-    name = "mcsema-source";
+    name = "mcsema-src";
   };
 }
