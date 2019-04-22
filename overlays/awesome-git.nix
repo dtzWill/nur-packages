eaa8c459c60471ad8e8d066ca23770bdb9ff92d9
Will Dietz <w@wdtz.org>
2019-04-21 21:06:37 -0500

    awesome: try fixing actions w/patch from PR comment: /

--- overlays/awesome-git.nix
+++ overlays/awesome-git.nix
@@ -11,6 +11,47 @@ self: super: {
       sha256 = "0z5lh1q50a2iawr1cc8nrk5kds2mxkink3kgawighgw7gsycx7dy";
     };
     buildInputs = (o.buildInputs or []) ++ [ self.xorg.xcbutilerrors ];
+    patches = (o.patches or []) ++ [
+     (builtins.toFile "notification-action-fix.patch" ''
+        From 007af75353484cbefe7611db07615fd0771757ac Mon Sep 17 00:00:00 2001
+        From: Will Dietz <w@wdtz.org>
+        Date: Sun, 21 Apr 2019 21:02:26 -0500
+        Subject: [PATCH] fix notification actions
+        
+        https://github.com/awesomeWM/awesome/pull/2740#issuecomment-480532583
+        ---
+         lib/naughty/action.lua | 7 +++++++
+         1 file changed, 7 insertions(+)
+        
+        diff --git a/lib/naughty/action.lua b/lib/naughty/action.lua
+        index a16a9570..b4907b41 100644
+        --- a/lib/naughty/action.lua
+        +++ b/lib/naughty/action.lua
+        @@ -86,6 +86,10 @@ for _, prop in ipairs { "name", "icon", "notification" } do
+             end
+         
+             action["set_"..prop] = function(self, value)
+        +        local old
+        +        if prop == "notification" then
+        +            old = self._private.notification
+        +        end
+                 self._private[prop] = value
+                 self:emit_signal("property::"..prop, value)
+         
+        @@ -93,6 +97,9 @@ for _, prop in ipairs { "name", "icon", "notification" } do
+                 if self._private.notification then
+                     self._private.notification:emit_signal("property::actions")
+                 end
+        +        if old then
+        +            old:emit_signal("property::actions")
+        +        end
+             end
+         end
+         
+        -- 
+        2.21.GIT
+     '')
+   ];
 
     #doCheck = true;
   });
