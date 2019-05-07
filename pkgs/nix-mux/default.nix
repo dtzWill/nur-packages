d99197c64e96301de98a6556cac12e49bf125e8a
Will Dietz <w@wdtz.org>
2019-05-07 16:12:01 -0500

    nix-mux: regen w/improved editline and other updates.

--- pkgs/nix-mux/default.nix
+++ pkgs/nix-mux/default.nix
@@ -4,8 +4,8 @@ let
   version = "2.3dtz-mux-ed8733d";
   nix-mux-tarball = lib.from-nar {
     name = "nix-${version}";
-    narurl = "nar/38ac9e18cbc963a0353a5b5a1a14bcf0bbbbf02d0fdde61ed185875f1b6bd31c.nar.xz";
-    narHash = "sha256:1x0b69q18ms2jc9qn5b4vdh5li5l3p6hhc5di3hf9l8p3b4hpi02";
+    narurl = "nar/49ab20f5c9d5f382779edd6bc29d0073a769341032568b816f0dcf305590ca3d.nar.xz";
+    narHash = "sha256:0a8ig0y8sl0hplys06csq3wqyk7y3yq1zpcg68nzd6ycy2gvxbs2";
   };
 
   bootstrapFiles = {
