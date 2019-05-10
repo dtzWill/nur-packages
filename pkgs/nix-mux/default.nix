8958f1655e3489b0bbf3c7e672ae2fb53168b8dc
Will Dietz <w@wdtz.org>
2019-05-10 15:33:00 -0500

    nix-mux: regen w/editline-git and nix repl ctrl-c change

--- pkgs/nix-mux/default.nix
+++ pkgs/nix-mux/default.nix
@@ -1,11 +1,11 @@
 { stdenv, runCommandNoCC, lib }:
 
 let
-  version = "2.3dtz-mux-dd888e6";
+  version = "2.3dtz-mux-f006c8a";
   nix-mux-tarball = lib.from-nar {
     name = "nix-${version}";
-    narurl = "nar/d20d8082187b27c12465778d0a993f1b5f5742ae42ce47b676c38830e3302047.nar.xz";
-    narHash = "sha256:093dcyj8a68ypa3458lxif7ysq527377wjd2332ral6vr01js4r5";
+    narurl = "nar/f0472aee4746899c721af16eb8e5aef52036574b41f92489204dda87becb259f.nar.xz";
+    narHash = "sha256:1gcs2psfx1d4bzgqyr0pqc3x2bmf12fggnrz2bzqjvf5g03p5msh";
   };
 
   bootstrapFiles = {
