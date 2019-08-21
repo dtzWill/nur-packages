839a599019fb20c2c108b634bde48e8bdcd3bffc
Will Dietz <w@wdtz.org>
2019-08-20 20:33:41 -0500

    tlp: git bump

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -2,13 +2,13 @@ self: super: {
   tlp = super.tlp.overrideAttrs (o: rec {
     name = "tlp-${version}";
     #version = "2019-04-28";
-    version = "2019-08-13";
+    version = "2019-08-18";
     src = super.fetchFromGitHub {
       owner = "linrunner";
       repo = "tlp";
-      rev = "1a28bc629b43d83760c6b20411c2be3135e64177";
+      rev = "81a99c0b531101c93f7953aedcf966bedca4e2de";
       #rev = version;
-      sha256 = "1h34yfjgzmwksxvr6634k32aa0argxmijfwc4wn3yx8cchplc1rn";
+      sha256 = "1h34yfjgzmwksxvr6634k32aa0argxmijfwc5wn3yx8cchplc1rn";
     };
   });
 }
