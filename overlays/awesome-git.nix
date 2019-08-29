aa418c0e6f299f853824d34ce8c20163d0434c3a
Will Dietz <w@wdtz.org>
2019-08-29 18:43:03 -0500

    awesome-git: bump

--- overlays/awesome-git.nix
+++ overlays/awesome-git.nix
@@ -3,12 +3,12 @@ self: super: {
   awesome = super.awesome.overrideAttrs (o: rec {
     pname = "awesome";
     name = "${pname}-${version}"; # override
-    version = "2019-08-12";
+    version = "2019-08-25";
     src = super.fetchFromGitHub {
       owner = "AwesomeWM"; # "dtzWill";
       repo = "awesome";
-      rev = "b2ebf899d79fe2a9c77b03c89d953beceb974efd";
-      sha256 = "0d6firh0hhibls6c7wa0rq5zw62sv59gr9kvdnkqps7np6n5di2p";
+      rev = "f1335be21ad0cbca90c63d9a25982904b966ac65";
+      sha256 = "1v5d4cx1jbhfk5zdccmvcaiypsly607s1ydf80y3hl5iyilnrh3n";
     };
     buildInputs = (o.buildInputs or []) ++ [ self.xorg.xcbutilerrors ];
 
