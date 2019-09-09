2b492b765a55ef7a23eb5759044fea60fb2f17dd
Will Dietz <w@wdtz.org>
2019-09-09 02:40:17 -0500

    enamel: update, poke a bit

--- pkgs/enamel/default.nix
+++ pkgs/enamel/default.nix
@@ -5,9 +5,9 @@ let
   nur-combined-src = fetchFromGitHub {
     owner = "nix-community";
     repo = "nur-combined";
-    # commit from: 2018-12-23
-    rev = "ba89a7a9a4a6cf4e1a9fd193ff577380d6fa3a86";
-    sha256 = "1b0yng19vshpzqqzqdh1ffcvawyb4pyjmmwwclmx99si2xzb148s";
+    # commit from: 2019-09-09
+    rev = "9f362f57ba709041d4a7ea5b5ecc5e7ffcff078d";
+    sha256 = "0vx50s0lpkhjd7hgrgi3hqddrfl1kj1f3070vyygrrm3gfyic539";
   };
   nur-combined = import "${nur-combined-src}" { inherit pkgs; };
   rust-nightly = nur-combined.repos.mozilla.latest.rustChannels.nightly.rust;
@@ -17,13 +17,13 @@ let
   };
 in rustPlatform.buildRustPackage rec {
   name = "enamel-${version}";
-  version = "2018-12-30";
+  version = "2019-09-09";
 
   src = fetchFromGitHub {
     owner = "vhdirk";
     repo = "enamel";
-    rev = "d09cd74cad2f3136bb1e31d9fde354eaf6cce76c";
-    sha256 = "01qz2c0x0zm4cp4rm8w8h605j62zsmzr0fqwkdyaw0w2xl91n4ia";
+    rev = "11f500f98e57e4d0a7248146138676c2941b20a3";
+    sha256 = "09h1wq346r9fyx984g45hfaz4bnwa41yam718v3dm8wxaig3cvws";
   };
 
   buildInputs = [ sassc glib gtk3 notmuch libsoup gmime3 webkitgtk ];
@@ -38,7 +38,6 @@ in rustPlatform.buildRustPackage rec {
   meta = with lib; {
     maintainers = with maintainers; [ dtzWill ];
     license = licenses.gpl3;
-    broken = true;
   };
 }
 
