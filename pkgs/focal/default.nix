d74b577b42514480de6a2ac4e3e3ac1182786fc8
Will Dietz <w@wdtz.org>
2019-09-24 23:29:55 -0500

    focal: localtime / libical TZ fixups

--- pkgs/focal/default.nix
+++ pkgs/focal/default.nix
@@ -15,6 +15,7 @@ stdenv.mkDerivation rec {
   patches = [
     ./dont-crash-if-calendar-has-vtodos-not-just-vevents.patch
     ./sync-timer.patch
+    ./localtime.patch
     (fetchpatch {
       name = "initially-showing-next-week-instead-of-current.patch";
       url = "https://github.com/ohwgiles/focal/pull/61.diff"; # diff not patch, don't want series
