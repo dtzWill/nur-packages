af53ac223ef336edee5b3db47172e0017cbc4152
Will Dietz <w@wdtz.org>
2019-10-04 15:48:39 -0500

    rofi-tab-switcher: init \o/

---
+++ pkgs/rofi-tab-switcher/default.nix
@@ -0,0 +1,43 @@
+{ stdenv, fetchFromGitHub, runCommand, writeText
+, rofi, python3 }:
+
+let
+  manifest = {
+    name = "rofi_interface";
+    description = "Native backend for rofi tab switcher.";
+    type = "stdio";
+    allowed_extensions = [ "@rofi.tab.switcher" ];
+    path = "${native_helper}/bin/rofiface.py";
+  };
+  native_helper = stdenv.mkDerivation rec {
+    pname = "rofi-tab-switcher";
+    version = "unstable-2019-09-07";
+
+    src = fetchFromGitHub {
+      owner = "blackhole89";
+      repo = pname;
+      rev = "92fbd93114a9058d6ae7c7cd339e299fd6c6d0b9";
+      sha256 = "19lh4jgqc3lmakwnqknsac1iq2dg8f5i8xl783vfm5aw57gvp48c";
+    };
+
+    buildInputs = [ python3 ];
+
+    dontBuild = true;
+
+    postPatch = ''
+      substituteInPlace rofiface.py \
+      --replace 'Popen("rofi ' \
+                'Popen("${rofi}/bin/rofi '
+    '';
+
+    # XXX: Maybe put into libexec instead, since not meant for PATH
+    installPhase = ''
+      install -Dm755 -t $out/bin rofiface.py
+    '';
+  };
+
+  plugin = runCommand "rofi-tab-switcher" {} ''
+    install -DT $out/lib/mozilla/native-messaging-hosts/rofi_interface.json \
+      ${writeText "rofi_interface.json" (builtins.toJSON manifest)}
+  '';
+in { inherit native_helper plugin; }
