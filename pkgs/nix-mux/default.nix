ecff566eabac8b9b63c24d4d6a710702427cdf69
Will Dietz <w@wdtz.org>
2019-10-23 13:37:08 -0500

    nix-mux: regen for minor bump

--- pkgs/nix-mux/default.nix
+++ pkgs/nix-mux/default.nix
@@ -1,12 +1,12 @@
 { stdenv, runCommandNoCC, lib }:
 
 let
-  versionSuffix = "dtz-mux-7441fce";
+  versionSuffix = "dtz-mux-0747e31";
   version = "2.4" + versionSuffix;
   nix-mux-tarball = lib.from-nar {
     name = "nix-${version}";
-    narurl = "nar/3c3d879e612a6403b3fd7ef1d5c4592a154b1e4b432d33349d5948b58d1a23b5.nar.xz";
-    narHash = "sha256:0fwgb1p9mhz1l9rm9p3aj2y2vdj080mcpb21zmxapbihnqbzb3as";
+    narurl = "nar/5e290cf40b75e5123eea4325cfc9876449cd1b04170ac684b7506d9e1b4a3fe7.nar.xz";
+    narHash = "sha256:1dzl9q9x633ww5c5xyb64xqmd3g91hhdgq9rgwpcvgasd4w8yyvh";
   };
 
   bootstrapFiles = {
