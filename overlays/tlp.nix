371c5e79ac443d9a95c7d367e046e2401b9be044
Will Dietz <w@wdtz.org>
2020-02-02 00:04:41 -0600

    tlp: disable, stable 1.3.0

--- overlays/tlp.nix
+++ overlays/tlp.nix
@@ -1,14 +1,14 @@
 self: super: {
-  tlp = super.tlp.overrideAttrs (o: rec {
-    name = "tlp-${version}";
-    version = "1.3.0";
-    #version = "2019-10-02";
-    src = super.fetchFromGitHub {
-      owner = "linrunner";
-      repo = "tlp";
-#      rev = "eca65630eca23f55e03dd67c165157b17fce470a";
-      rev = version;
-      sha256 = "1bgx9psgx9izvhi0y76ayik6ymxsbj3rb9a0y4l1sxx1b4smixg8";
-    };
-  });
+##  tlp = super.tlp.overrideAttrs (o: rec {
+##    name = "tlp-${version}";
+##    version = "1.3.0";
+##    #version = "2019-10-02";
+##    src = super.fetchFromGitHub {
+##      owner = "linrunner";
+##      repo = "tlp";
+###      rev = "eca65630eca23f55e03dd67c165157b17fce470a";
+##      rev = version;
+##      sha256 = "1bgx9psgx9izvhi0y76ayik6ymxsbj3rb9a0y4l1sxx1b4smixg8";
+##    };
+##  });
 }
