28c29f95c7e7c6200ec337baca7878dd85b49c25
Will Dietz <w@wdtz.org>
2020-07-13 15:18:45 -0500

    joycontrol: install driver script, wrap w/joycontrol lib package

--- pkgs/joycontrol/default.nix
+++ pkgs/joycontrol/default.nix
@@ -1,42 +1,57 @@
-{ python3, fetchFromGitHub, lib, hidapi }:
+{ stdenv, python3, fetchFromGitHub, lib, hidapi }:
 
 let
+  common = rec {
+    pname = "joycontrol";
+    version = "unstable-2020-07-11";
+    src = fetchFromGitHub {
+      owner = "mart1nro";
+      repo = pname;
+      rev = "e83dc5964b321daf9f02fea155f1dfdf394aec8b";
+      sha256 = "0s2aknsssafk7riyzp71iblb7rq7myyg1aq51jc5jfdqkd3ljrnb";
+    };
+  };
   py3 = python3.override {
     packageOverrides = self: super: {
       crc8 = import ./py-crc8.nix {
-        inherit (super) fetchPypi buildPythonPackage;
+        inherit (self) fetchPypi buildPythonPackage;
         inherit lib;
       };
       hid = import ./py-hid.nix {
-        inherit (super) fetchPypi buildPythonPackage;
+        inherit (self) fetchPypi buildPythonPackage;
         inherit lib;
 
         # test deps
-        inherit (super) nose;
+        inherit (self) nose;
         inherit hidapi;
       };
+
+      ## library
+      joycontrol = self.buildPythonPackage (common // {
+        # dbus-python is correctly passed in propagatedBuildInputs, but for some reason setup.py complains.
+        # The wrapped terminator has the correct path added, so ignore this.
+        postPatch = ''
+            substituteInPlace setup.py --replace "'dbus-python'," ""
+        '';
+
+        # Fails loading hidapi libs? :(
+        doCheck = false;
+
+        propagatedBuildInputs = with py3.pkgs; [ hid aioconsole dbus-python crc8 ];
+      });
     };
   };
 in
-py3.pkgs.buildPythonApplication rec {
-  pname = "joycontrol";
-  version = "unstable-2020-07-11";
 
-  src = fetchFromGitHub {
-    owner = "mart1nro";
-    repo = pname;
-    rev = "e83dc5964b321daf9f02fea155f1dfdf394aec8b";
-    sha256 = "0s2aknsssafk7riyzp71iblb7rq7myyg1aq51jc5jfdqkd3ljrnb";
-  };
+stdenv.mkDerivation (common // {
+  dontBuild = true;
+  installPhase = ''
+    runHook preInstall
 
-  # dbus-python is correctly passed in propagatedBuildInputs, but for some reason setup.py complains.
-  # The wrapped terminator has the correct path added, so ignore this.
-  postPatch = ''
-      substituteInPlace setup.py --replace "'dbus-python'," ""
+    install -Dm755 -t $out/bin run_controller_cli.py
+
+    runHook postInstall
   '';
 
-  # Fails loading hidapi libs? :(
-  doCheck = false;
-
-  buildInputs = with py3.pkgs; [ hid aioconsole dbus-python crc8 ];
-}
+  buildInputs = [ (py3.withPackages (ps: with ps; [ joycontrol ])) ];
+})
