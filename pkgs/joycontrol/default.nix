d31a43dcac0f0a248c4acaa2986ecb15064c75ee
Will Dietz <w@wdtz.org>
2020-07-13 14:36:26 -0500

    joycontrol: python deps, add crc8 locally for now

--- pkgs/joycontrol/default.nix
+++ pkgs/joycontrol/default.nix
@@ -1,6 +1,16 @@
-{ python3, fetchFromGitHub }:
+{ python3, fetchFromGitHub, lib }:
 
-python3.pkgs.buildPythonApplication rec {
+let
+  py3 = python3.override {
+    packageOverrides = self: super: {
+      crc8 = import ./py-crc8.nix {
+        inherit (super) fetchPypi buildPythonPackage;
+        inherit lib;
+      };
+    };
+  };
+in
+py3.pkgs.buildPythonApplication rec {
   pname = "joycontrol";
   version = "unstable-2020-07-11";
 
@@ -10,4 +20,6 @@ python3.pkgs.buildPythonApplication rec {
     rev = "e83dc5964b321daf9f02fea155f1dfdf394aec8b";
     sha256 = "0s2aknsssafk7riyzp71iblb7rq7myyg1aq51jc5jfdqkd3ljrnb";
   };
+
+  buildInputs = with py3.pkgs; [ hidapi aioconsole dbus-python crc8 ];
 }
